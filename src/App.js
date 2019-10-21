855b6062e7f4828baae1b435c3c43aff44e3a2ea
Riandy Dimas B <riandy.banimahendra@airyrooms.com>
2019-10-22 05:50:20 +0700

    feat: add search input field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Grommet } from 'grommet';
+import React, { useState, useEffect } from 'react';
+import { Box, Grommet, Heading, TextInput } from 'grommet';
 
 const theme = {
   global: {
@@ -28,10 +28,42 @@ const AppBar = (props) => (
   />
 );
 
-function App() {
+const SearchField = (props) => {
+  const { value, onChange } = props
   return (
-    <Grommet theme={theme}>
-      <AppBar>Multiplex Search</AppBar>
+    <TextInput
+      placeholder="search something"
+      value={value}
+      onChange={event => onChange(event.target.value)}
+    />
+  );
+}
+
+function App() {
+  const [query, setQuery] = useState('');
+
+  return (
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level='3' margin='none'>Multiplex Search</Heading>
+        </AppBar>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box
+              width='small'
+              background='light-2'
+              elevation='small'
+              align='center'
+              justify='top'
+              pad='small'
+            >
+            <SearchField value={query} onChange={(text) => { setQuery(text) }} />
+          </Box>
+          <Box flex align='center' justify='center'>
+            Main body
+          </Box>
+        </Box>
+      </Box>
     </Grommet>
   );
 }
